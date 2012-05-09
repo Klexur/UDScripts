88113d31057971d91d6078309ab37a8758069433
Klexur <klexur@gmail.com>
2012-05-09 14:44:51 -0500

    Added page load function, changed function names to make more sense. Changed button to a link with the href as the javascript. 'window.loacation.replace' still not working in Chrome.

--- UD_PK_Reporter.user.js
+++ UD_PK_Reporter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           UD PK Reporter
 // @namespace      Klexur
-// @version        0.2
+// @version        0.2.1
 // @description    *Work In Progress* Automates the task of submitting a Dumbwit url to the Rogues Gallery
 // @include        http://www.urbandead.com/map.cgi*
 // @include        http://*urbandead.com/map.cgi*
@@ -12,57 +12,43 @@
 // ==/UserScript==
 
 var durl = document.location.href
-
 if (durl.indexOf('urbandead.com/map.cgi') != -1) addButton();
-else if (durl.indexOf('iamscott.net/cgi-bin/dumbwit.rb') != -1) openReport();
-else if (durl.indexOf('rg.urbandead.net/reports/add/url#') != -1) sendReport();
+
+// Wait for page to load
+window.addEventListener('load', function() {
+	if (durl.indexOf('iamscott.net/cgi-bin/dumbwit.rb') != -1) getLink();
+	if (durl.indexOf('rg.urbandead.net/reports/add/url#') != -1) openReport();
+}, true);
 
 function addButton() {
-	var input = document.createElement('input');
-	input.type = 'submit';
-	input.className = 'y';
-	input.value = 'Report PK';
-	input.style.color = '#FF9999';
-	input.addEventListener(
-		'click',
-		function(event) {
-			event.stopPropagation();
-			event.preventDefault();
-			var d = new Date();
-			var w = window.open('', d);
-			w.document.write('<html><body><form name="wF" action="http://iamscott.net/cgi-bin/dumbwit.rb" method="post"><input name="wP" value="PRIVATE" /><input name="wC" value="' + prompt('Enter Dumbwit comment - may be blank.') + '"><input name="wT" value="' + window.document.lastModified + '" /><input name="wZ" value="' + d.getTimezoneOffset() + '" /><input name="wV" value="23" /><textarea name="wS">' + document.body.innerHTML + '</textarea></form>');
-			w.document.forms[0].submit();
-		},
-		false
-	);
+	var a = document.createElement('a');
+	a.className = 'y';
+	a.href = "javascript:d=new%20Date();void(wW=window.open('',%20d.toString().replace(/[\W]/g,%20'')));void(wW.document.write(%22<HTML><BODY><FORM%20NAME='wF'%20ACTION='http://iamscott.net/cgi-bin/dumbwit.rb'%20METHOD='POST'><INPUT%20NAME='wP'%20VALUE='PRIVATE'><INPUT%20NAME='wC'%20VALUE='%22+prompt(%22enter%20Dumbwit%20comment%20-%20may%20be%20blank%22)+%22'><INPUT%20NAME='wT'%20VALUE='%22+window.document.lastModified+%22'><INPUT%20NAME='wZ'%20VALUE='%22+d.getTimezoneOffset()+%22'><INPUT%20NAME='wV'%20VALUE='23'><TEXTAREA%20NAME='wS'>%22+document.body.innerHTML+%22</TEXTAREA>%22));u=window.location.href.indexOf(%22ead.com/map.c%22);if(12<u&&u<18)wW.document.wF.submit()";
+	a.innerHTML = 'Report PK';
 	
 	var frag = document.createDocumentFragment();
 	frag.appendChild(document.createTextNode(' '));		// seems to create the equivalent of &nbsp;
-	frag.appendChild(input);
+	frag.appendChild(a);
 	frag.appendChild(document.createTextNode(' '));
 	
-	var firstP = document.evaluate('//td[@class="cp"]/p/a', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null).snapshotItem(0);
-	if (!firstP)
+	var firstA = document.evaluate('//td[@class="cp"]/p/a', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null).snapshotItem(0);
+	if (!firstA)
 		document.evaluate('//td[@class="cp/p"]', document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null).snapshotItem(0).appendChild(frag);
 	else
-		// Place before the Log out link
-		firstP.parentNode.insertBefore(frag, firstP.parentNode.lastChild.previousSibling);
+		// Place before the 'Log out' link
+		firstA.parentNode.insertBefore(frag, firstA.parentNode.lastChild.previousSibling);
 }
 
-function openReport() {
+function getLink() {
 	var a = document.getElementsByTagName('a');
-	var link = a[0].href; // assuming Dumbwit capture is first link
-	// GM_log('Dumbwit link: ' + link);
-	console.log('Dumbwit link: ' + link);
-		
-	// GM_openInTab('http://rg.urbandead.net/reports/add/url#' + link);
-	//window.open('http://rg.urbandead.net/reports/add/url#' + link, '', '', true);
-	window.onLoad = a[0].focus();
+	// window.onLoad = a[0].focus();
+	// Assuming Dumbwit capture is first link
+	var link = a[0].href;
 	window.location.replace('http://rg.urbandead.net/reports/add/url#' + link);
 	//copyToClipboard(link);
 }
 
-function sendReport() {
+function openReport() {
 	var i = durl.indexOf('#');
 	if (i == -1) return;
 	
@@ -70,6 +56,7 @@ function sendReport() {
 	var upinput = document.getElementById('DocumentUrl');
 	window.onLoad = upinput.focus();
 	upinput.value = link;
+
 	// var upform = document.getElementById('ObservationsAddForm');
 	// upform.submit();
 }
