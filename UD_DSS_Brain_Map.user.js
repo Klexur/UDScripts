cf1a0e6161ac9f4dd9c33c59feb6575d3fa07400
Klexur <klexur@gmail.com>
2012-06-12 22:40:35 -0500

    0.2.0 Fixed single digit coordinates, added report age

--- UD_DSS_Brain_Map.user.js
+++ UD_DSS_Brain_Map.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name           UD DSS Brain Map
 // @namespace      Klexur
-// @version        0.1.3
+// @version        0.2.0
 // @description    Displays the UDBrain data on the DSS map
 // @updateURL      https://github.com/Klexur/UDScripts/raw/master/UD_DSS_Brain_Map.user.js
 // @include        http://dssrzs.org/map/*
@@ -10,26 +10,34 @@
 
 // Ben2's UDBrainMap script used as reference and some code borrowed.
 
-window.addEventListener('load', getCoords(), true);
+window.addEventListener('load', getBlocks(), true);
 
-function getCoords() {
-	var query = '//table[@class="map"]//td//div[@class="p"]';
+function getBlocks() {
+	var query = '//table[@class="map"]//td[contains(@class,loc)]';
 	var blocks = document.evaluate(query, document, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
-	//console.log('blocks.snapshotLength: ' + blocks.snapshotLength);
+	unsafeWindow.console.log('Brain blocks.snapshotLength: ' + blocks.snapshotLength);
 	
 	for (var i = 0; i < blocks.snapshotLength; i++) {
-		var currentBlock = blocks.snapshotItem(i).parentNode;
+		var currentBlock = blocks.snapshotItem(i).firstChild;
 		// Non Free-running: carpark cemetary monument park street wasteland zoo
     	if ((currentBlock.className!='loc cprk')&&(currentBlock.className!='loc ceme')&&
 			(currentBlock.className!='loc monu')&&(currentBlock.className!='loc park')&&
 			(currentBlock.className!='loc opns')&&(currentBlock.className!='loc wast')&&
 			(currentBlock.className!='loc zoox')) { 
-			var coordinates = blocks.snapshotItem(i).innerHTML.replace(',','');
-			//console.log('coordinates: ' + coordinates);
+			var coordinates = getCoords(blocks.snapshotItem(i).id);
 			getData(coordinates, currentBlock);
 		}
 	}
 }
+
+function getCoords(id) {
+	var a = id.substr(1).split('y');
+	var x = a[0];
+	if (a[0] <= 9) x = '0' + a[0];
+	var y = a[1];
+	if (a[1] <= 9) y = '0' + a[1];
+	return (x + y);
+} 
 
 function getData(building, currentBlock) {
 	GM_xmlhttpRequest({
@@ -38,10 +46,8 @@ function getData(building, currentBlock) {
 		onload: function(response) {
 			if(response.responseText != '') {
 				var data = response.responseText;
-				//console.log(data);
 				var arr = data.split(':');
 				// arr[0] = coords, arr[1] = timestamp, arr[2] = report type, arr[3] = barricades
-				// addData(arr[0], arr[1], arr[2], arr[3]);
 				addData(currentBlock, arr[1], arr[2], arr[3]);
 			}
 		}
@@ -52,6 +58,7 @@ function addData(currentBlock, timestamp, rtype, barricades) {
 	var cades = convertCadeLevelToShort(barricades);
 	var description = currentBlock.firstChild.nextSibling;
 	description.innerHTML = description.innerHTML + '<br>' + cades;
+	description.title = getAge(timestamp);
 }
 
 function convertCadeLevelToShort(cl) {
@@ -64,4 +71,25 @@ function convertCadeLevelToShort(cl) {
 	if(cl == 7) return "(HeB)";
 	if(cl == 8) return "(VHB)";
 	if(cl == 9) return "(EHB)";
+}
+
+function getAge(seconds) {
+	var minutes = Math.floor(seconds / 60);
+	var hours = Math.floor(minutes / 60);
+	var days = Math.floor(hours / 24);
+	
+	// Handle new time granularity
+	var agestr = '';
+	if(seconds == 3600) agestr = '<1h';
+	else if(seconds == 14400) agestr = '<4h';
+	else {							
+		seconds %= 60;
+		minutes %= 60;
+		hours %= 24;
+		if (days) agestr += days + 'd';
+		if (hours) agestr += hours + 'h';
+		if (minutes) agestr += minutes + 'm';
+		if (seconds) agestr += seconds + 's';
+	}
+	return agestr;
 }
